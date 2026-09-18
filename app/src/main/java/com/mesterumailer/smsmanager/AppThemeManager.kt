056483594c1b78538c31c456f3dceea5c465632f
mesterumailer <mesterumailer@gmail.com>
2026-09-18 03:34:55 +0330

    feat: add activity theme application helper

---
+++ app/src/main/java/com/mesterumailer/smsmanager/AppThemeManager.kt
@@ -0,0 +1,31 @@
+package com.mesterumailer.smsmanager
+
+import android.app.Activity
+import android.view.View
+import com.mesterumailer.smsmanager.data.AppTheme
+import com.mesterumailer.smsmanager.data.ThemePreferenceRepository
+
+object AppThemeManager {
+    fun applySavedTheme(activity: Activity) {
+        val theme = ThemePreferenceRepository(activity).getTheme()
+        activity.setTheme(
+            when (theme) {
+                AppTheme.LIGHT -> R.style.AppTheme
+                AppTheme.DARK -> R.style.AppTheme_Dark
+            }
+        )
+    }
+
+    fun configureWindow(activity: Activity) {
+        val dark = ThemePreferenceRepository(activity).getTheme() == AppTheme.DARK
+        val background = activity.getColor(R.color.page_background)
+        activity.window.statusBarColor = background
+        activity.window.navigationBarColor = background
+        activity.window.decorView.systemUiVisibility =
+            if (dark) {
+                0
+            } else {
+                View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR or View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR
+            }
+    }
+}
