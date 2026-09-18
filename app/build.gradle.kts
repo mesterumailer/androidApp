fb1d1f45779158d891529f073ee10a49f1b24407
mesterumailer <mesterumailer@gmail.com>
2026-09-18 15:57:18 +0330

    release: start UI polish milestone 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mesterumailer.smsmanager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.4.4"
+        versionCode = 9
+        versionName = "0.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
