08f7ec0388d10251edbd5bd368985fc800ffa39e
mesterumailer <mesterumailer@gmail.com>
2026-09-18 04:37:08 +0330

    chore: bump patch version for theme fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mesterumailer.smsmanager"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
